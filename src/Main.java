1925c336b58f919a46a67aaa1893a4dee0aac8a2
Christian <Christianmnaira@gmail.com>
2024-12-12 22:07:17 +0800

    feat: added a method to store an initial user account

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,20 @@ public class Main {
 
         ArrayList<ArrayList<String>> userAccounts = new ArrayList<>();
 
+        storeAccounts(userAccounts);
+
+        System.out.println(userAccounts);
+
+    }
+
+    static void storeAccounts(ArrayList<ArrayList<String>> userAccounts) {
+
+        ArrayList<String> user1 = new ArrayList<>();
+        user1.add("Christian Naira");
+        user1.add("1234");
+        user1.add("10000");
+
+        userAccounts.add(user1);
 
     }
 
