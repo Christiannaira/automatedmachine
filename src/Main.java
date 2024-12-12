6506f4be232954cc58b94efd4748b5ef276c2ccf
Christian <Christianmnaira@gmail.com>
2024-12-12 22:04:12 +0800

    feat: added array list of user accounts

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
+
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("hello wolrd");
+        ArrayList<ArrayList<String>> userAccounts = new ArrayList<>();
+
 
     }
+
 }
